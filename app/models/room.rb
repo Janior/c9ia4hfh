f7e3ea06a5dae3bd61d3b8817bca6089ae43164c
Janior <janior_orlando@hotmail.com>
2018-08-13 16:40:23 -0700

    Validaciones para rooms actualizar 1

--- app/models/room.rb
+++ app/models/room.rb
@@ -11,9 +11,10 @@
 #  updated_at  :datetime         not null
 #  image_url   :string
 #
+# :price_per_night,
 
 class Room < ActiveRecord::Base
-	validates :title, :description, :beds, :guests, :image_url, :price_per_night, presence: true
+	validates :title, :description, :beds, :guests, :image_url,  presence: true
 	validates :description, length: {maximum: 400}
 	validates :beds, numericality: { only_integer: true}
 	validates :guests, numericality: { only_integer: true}
