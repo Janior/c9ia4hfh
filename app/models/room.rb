da2e02433cb75ebb18bdccce69cb05e923e1eb78
Janior <janior_orlando@hotmail.com>
2018-08-13 12:39:16 -0700

    Validaciones para rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,11 @@
 #
 
 class Room < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 400, too_long: "%{count} characters is the maximum allowed" }
+  validates :beds, presence: true, numericality: {only_integer: true}
+  validates :guests, presence: true, numericality: {only_integer: true}
+  #validates :created_at, presence: true
+  #validates :updated_at, presence: true
+  validates :image_url, presence: true
 end
